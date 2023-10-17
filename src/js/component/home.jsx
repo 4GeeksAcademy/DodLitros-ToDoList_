83b72e48476dbd165be1c94654765d5f64363d9e
Carlos beltran <137569880+DodLitros@users.noreply.github.com>
2023-10-17 16:44:17 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+
+	const [task, setTask] = useState("");
+	const [saveTask, setSaveTask] = useState([]);
+
+		function handleTask(event){
+			setTask(event.target.value)
+		}
+
+		function handleTaskList (event){
+			if (event.key == "Enter"){
+				if (task.trim() !== ""){
+					setSaveTask([...saveTask, task.trim()]);
+					setTask("");	
+				}
+			}
+			
+		}
+		function handleDelete (index) {
+			let filterTask = saveTask.filter((_, indexFilter) => index !== indexFilter)
+			setSaveTask(filterTask)
+		}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container d-flex flex-column justify-content-center">
+			<div className="text-center">
+				<div className="row justify-content-center">
+					<input onChange={ handleTask }  value={ task } onKeyDown={ handleTaskList } type="text" className="w-100 rounded border-secondary" placeholder="Añadir Tarea"/>
+				</div>
+					<ul clas>
+					{
+						saveTask.map((item,index)=>{
+							return(
+								<li key={ index } className="border-bottom d-flex justify-content-between">
+									 <span> { item } </span>  <i onClick={ ()=> handleDelete(index) }  className="fas fa-trash"></i>
+								</li>
+								)
+							})
+						}
+					</ul>
+			</div>
 		</div>
 	);
 };
